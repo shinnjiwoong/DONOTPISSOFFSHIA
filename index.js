503dfd24ab94bf7be59d3202ffa928ab77373726
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2023-04-17 11:15:26 +0900

    230417 Dev
    Add Codes

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
+// ENDNOTE 
+const endnote_kr = 'ENDNOTE: <br />SNS에서는 모두가 슈퍼스타처럼 보인다. <br />‘나’를 제외한 모두가 의미있는 일들을 하며 멋지게 앞으로 나아가는 것만 같다. <br />‘나’에게도 분명 반짝이듯 머리에 떠올랐던 그 ‘일’이 있었던 것 같다. <br />다만, 이미 잘나가는 수많은 사람들을 보면 내가 떠올렸던 그 아이디어들은 하찮아보이기 시작하고, 이내 그 반짝임은 사라진다. 그렇게 우리는 상상했던 그 ‘일’은 수많은 의심과 불안과 ‘그럴 여유가 없다’는 이유로 행동으로 옮겨지지 못한다. <br />너무나 흔한 일이고, 질타받아야 할 일도 아니다. <br />다만… <br />샤이아 라보프는 이러한 우리를 보며 속으로 분노를 삭인다. <br />분노를 삭이고 삭이다 그는 결국 참지 못하고 터뜨리며 소리친다. <br />DO IT! JUST DO IT! <br />분노를 터뜨리는 샤이아 라보프를 보면 저러다 혈압이 터져 쓰러지는 건 아닐까 하는 걱정이 든다. <br />이내 그의 화를 진정시키며 달래고 싶은 마음이 든다. <br />그의 분노를 듣다보면 그가 원하는 건 단 한가지인 것 같다. <br />우리가 주저했던 그 ‘일’을 더이상은 망설이지 않고 하는 것. <br />이 웹사이트에서 그의 분노에 귀기울여 보자. <br />그리고 오늘 하루만이라도 그의 화를 달래줘보자. 사람 한명 살리는 셈 치고. <br />우리가 각자 미뤄왔던 그것을 조금이라도 한다면, 그는 금방 온순해질 것이다.';
+const endnote_en = 'ENDNOTE: <br />People in SNS world are all super stars. <br>Everyone seems to go on achieving meaningful works except ‘me’. <br>I also had one of those beautiful ideas of work. <br>But, feelings of doubt, anxiety and lack of time stopped me from actually working on it. <br>This happens to all of us and it’s not something to be blamed at. <br>But….<br>Shia LaBeouf gets mad upon us. <br>He’s getting pissed off and shouting at us like <br>”DO IT! JUST DO IT!”<br>Let’s listen to his anger in this website, and calm him down by doing something what we procrastinated TODAY. <br>Not for ourselves, but, for saving Shia’s mental peace. '
+const endnoteKrBtn = document.getElementById('kr')
+const endnoteEnBtn = document.getElementById('en')
+const endnote = document.getElementById('endnote');
+
+endnoteKrBtn.addEventListener('click', ()=>{
+    endnote.innerHTML = endnote_kr;
+})
+endnoteEnBtn.addEventListener('click', ()=>{
+    endnote.innerHTML = endnote_en;
+})
+
 
 // Intro Canvas
 const canvas = document.querySelector('canvas')
@@ -107,6 +121,7 @@ const doorRight = document.getElementById('door-right');
 const scriptBtn = document.getElementById('script-btn');
 
 introBtn.addEventListener('click', ()=>{
+    introShia.style.opacity = '0%';
     introBtn.style.opacity = '0%'
     introBtn.style.pointerEvents = 'none';
     document.body.style.overflowY = 'scroll';
@@ -114,9 +129,10 @@ introBtn.addEventListener('click', ()=>{
     cursor.style.display = 'none';
     doorLeft.style.transform = 'translateX(-100%)'
     doorRight.style.transform = 'translateX(100%)'
+    endnoteBtn.style.display = 'inline-block'
     playHaptic();
     // Set the scroll speed factor
-    let scrollSpeed = 0.07;
+    let scrollSpeed = 0.1;
 
     // Add an event listener for the 'wheel' event
     document.addEventListener('mousewheel', function(event) {
@@ -140,6 +156,7 @@ introBtn.addEventListener('click', ()=>{
             e.style.opacity = '100%'
         })
         scriptBtn.style.opacity = '100%';
+        introShia.style.display = 'none'
     }, 800)
 
     setTimeout(() => {
@@ -223,10 +240,20 @@ function animate(){
     then = now - (delta % interval);
 }
 
+const introShia = document.getElementById('intro-shia');
+
 window.addEventListener('load', () => {
     window.scrollTo(0, 0);
     init();
     animate();
+    setInterval(function(){
+        if(introShia.classList.contains('up')){
+            introShia.classList.remove('up');
+        }else{
+            introShia.classList.add('up');
+        }
+        
+    }, 2500)
 })
 
 window.addEventListener('resize', ()=>{
@@ -239,4 +266,17 @@ window.addEventListener('mousemove', (e) => {
     cursor.style.top = e.clientY + 'px';
     cursor.style.left = e.clientX + 'px';
 })
+
+const endnoteBtn = document.getElementById('header-endnote-btn');
+const endnoteWrapper = document.getElementById('endnote-wrapper');
+const endnoteEsc = document.getElementById('esc-btn');
+
+endnoteBtn.addEventListener('click', ()=>{
+    endnoteWrapper.style.transform = 'translateX(0%)';
+})
+
+endnoteEsc.addEventListener('click', ()=>{
+    endnoteWrapper.style.transform = 'translateX(100%)';
+})
+
 
