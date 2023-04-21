017ec75b617d9995c6bffb3eeb9ec913da5fa0df
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2023-04-21 13:05:35 +0900

    230421 Dev
    Add codes for video

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 // DOM 
 const endnote_kr = 'ENDNOTE: <br />이 웹사이트는 샤이아 라보프의 분노를 포함하고 있습니다. <br>그의 분노는 미래에 대한 불안과 자기 자신에 대한 의심으로 인해 빛바래져가는 우리의 수많은 아이디어를 그대로 두지 말고 지금 당장 뛰어들라고 말합니다. <br>당신의 아름다운 아이디어는 무엇입니까? <br>한때 빛이 났었지만, 미뤄오는 동안 점점 바래져가던 아이디어를 다시 쳐다보지 않는다면, 샤이아의 분노는 점점 더 커질 것입니다.';
 const endnote_en = 'ENDNOTE: <br />This website contains Shia LaBeouf’s rage. <br>His anger shouts at us to dive into our beautiful ideas that we procrastinated, due to the anxiety about the future and self-doubt. <br>What was your beautiful idea? <br>If you keep ignoring your idea that was once bright, Shia will be pissed off even more. '
+const script_kr = '해. 그냥 좀 해.<br>너의 꿈을 꿈인 채로 두지 좀 마.<br>어제 너는 내일 한다고 했잖아.<br>그러니까 제발 그냥 좀 해.<br>네 꿈을 실현시키라고.<br>그냥 좀 해.<br>어떤 이들은 성공을 꿈꾸는 동안, 너는 깨어나서 그걸 위해 일하라고.<br>그 어떤 것도 가능해.<br>너는 다른 사람들이 다 포기하는 지점까지 가서 멈추지 않고 계속 하는거야.<br>아니야, 뭘 기다리는거야?<br>그냥 해.<br>그냥 좀 해.<br>할 수 있다고.<br>그냥 좀 해.<br>다시 시작하는것이 지겹다면, 포기하지를 마.';
+const script_en = "Do it. Just do it. <br />Don't let your dreams be dreams. <br />Yesterday you said tomorrow. <br />So just do it. <br />Make your dreams come true. <br />Just do it. <br />Some people dreamof success, <br />while you're gonna wake up and work hard at it. <br />Nothing is impossible. <br />You should get to the point where anyone else would quit, <br />and you're not going to stop there. <br />No, what are you waiting for? <br />Do it. <br />Just do it. <br />Yes you can. <br />Just do it. <br />If you're tired of starting over STOP GIVING UP.";
 const endnoteKrBtn = document.getElementById('kr')
 const endnoteEnBtn = document.getElementById('en')
 const endnote = document.getElementById('endnote');
 const doorLeft = document.getElementById('door-left');
 const doorRight = document.getElementById('door-right');
 const scriptBtn = document.getElementById('script-btn');
+const scriptKr = document.getElementById('script-kr-btn')
+const scriptEn = document.getElementById('script-en-btn')
+const script = document.getElementById('script')
+const poemBtn = document.getElementById('script-header-btn');
 const topBtn = document.getElementById('top-btn')
 const vocalBtns = document.querySelectorAll('.vocal-btn');
 const shiaDown = document.getElementById('shia-down');
@@ -16,6 +22,7 @@ const endnoteBtn = document.getElementById('header-endnote-btn');
 const endnoteWrapper = document.getElementById('endnote-wrapper');
 const endnoteEsc = document.getElementById('esc-btn');
 const cursor = document.getElementById('cursor-wrapper');
+
 
 // CANVAS 
 const canvas = document.querySelector('canvas')
@@ -42,19 +49,16 @@ class Vocal {
     }
     play(){
         this.audioDuration = Math.ceil(this.audio.duration);
-        console.log(`playing ${this.name}`)
-        console.log(this.audio)
-        console.log(`duration : ${this.audioDuration}`);
-        // this.audio.play()
+        this.audio.play()
     }
 }
 
-const vocal_1 = new Vocal(vocalBtns[0].innerText, 'src/audio/cursor_haptic.mp3');
-const vocal_2 = new Vocal(vocalBtns[1].innerText, 'src/audio/cursor_haptic.mp3');
-const vocal_3 = new Vocal(vocalBtns[2].innerText, 'src/audio/cursor_haptic.mp3');
-const vocal_4 = new Vocal(vocalBtns[3].innerText, 'src/audio/cursor_haptic.mp3');
-const vocal_5 = new Vocal(vocalBtns[4].innerText, 'src/audio/cursor_haptic.mp3');
-const vocal_6 = new Vocal(vocalBtns[5].innerText, 'src/audio/cursor_haptic.mp3');
+const vocal_1 = new Vocal(vocalBtns[0].innerText, 'src/audio/shia_doit.mp3');
+const vocal_2 = new Vocal(vocalBtns[1].innerText, 'src/audio/shia_justdoit.mp3');
+const vocal_3 = new Vocal(vocalBtns[2].innerText, 'src/audio/shia_dreams.mp3');
+const vocal_4 = new Vocal(vocalBtns[3].innerText, 'src/audio/shia_possible.mp3');
+const vocal_5 = new Vocal(vocalBtns[4].innerText, 'src/audio/shia_wait.mp3');
+const vocal_6 = new Vocal(vocalBtns[5].innerText, 'src/audio/shia_yes.mp3');
 
 const vocals = [vocal_1, vocal_2, vocal_3, vocal_4, vocal_5, vocal_6];
 
@@ -194,20 +198,20 @@ introBtn.addEventListener('click', ()=>{
     let scrollSpeed = 0.1;
 
     // Add an event listener for the 'wheel' event
-    document.addEventListener('mousewheel', function(event) {
+    // document.addEventListener('mousewheel', function(event) {
 
-        // Prevent default scrolling behavior
-        event.preventDefault();
+    //     // Prevent default scrolling behavior
+    //     event.preventDefault();
 
-        // Calculate the new scroll position
-        let delta = event.deltaY;
-        let scrollPosition = window.scrollY + (delta * scrollSpeed); 
+    //     // Calculate the new scroll position
+    //     let delta = event.deltaY;
+    //     let scrollPosition = window.scrollY + (delta * scrollSpeed); 
 
-        // Set the new scroll position
-        window.scrollTo({
-          top: scrollPosition
-        });
-    }, {passive:false});
+    //     // Set the new scroll position
+    //     window.scrollTo({
+    //       top: scrollPosition
+    //     });
+    // }, {passive:false});
 
 
     setTimeout(()=>{
@@ -218,9 +222,6 @@ introBtn.addEventListener('click', ()=>{
         introShia.style.display = 'none'
     }, 800)
 
-    setTimeout(() => {
-        playShia();
-    }, 1000)
 })
 
 
@@ -291,7 +292,61 @@ function animate(){
     then = now - (delta % interval);
 }
 
+// Event Listeners
 
+// 비디오 스크롤 재생
+const playback = 200;
+const video = document.getElementById('video');
+const videoFixedWrapper = document.getElementById('video-fixed-wrapper');
+const videoSection = document.getElementById('video-section');
+
+window.addEventListener('scroll', ()=>{
+    centerEl('video-fixed-wrapper', video);
+
+    if(window.scrollY > videoSection.offsetTop + videoSection.offsetHeight - (videoFixedWrapper.offsetHeight + (document.documentElement.clientHeight - videoFixedWrapper.offsetHeight)/2)){
+        videoFixedWrapper.style.position = 'relative';
+        videoFixedWrapper.style.top = 'initial';
+        videoFixedWrapper.style.left = 'initial';
+        videoFixedWrapper.style.transform = `translateY(${videoSection.offsetHeight - videoFixedWrapper.offsetHeight}px)`;
+    }
+})
+
+function centerEl(elementId){
+    const element = document.getElementById(elementId);
+    const parentElement = element.parentElement;
+
+    if(window.scrollY > parentElement.offsetTop - ((document.documentElement.clientHeight - element.offsetHeight) / 2)){
+        element.style.position = 'fixed';
+        element.style.top = '50%';
+        element.style.left = '50%';
+        element.style.transform = 'translate(-50%, -50%)';
+
+        // if(video) video.currentTime = (window.scrollY - videoSection.offsetTop) / playback
+    }else{
+        element.style.position = 'relative';
+        element.style.top = 'initial';
+        element.style.left = 'initial';
+        element.style.transform = 'initial';
+    }
+
+}
+
+video.addEventListener('loadedmetadata', ()=>{
+    document.getElementById('video-section').style.height = video.duration * playback + 'px';
+
+})
+
+poemBtn.addEventListener('click', ()=>{
+    shia_full.play()
+})
+
+scriptKr.addEventListener('click', ()=>{
+    script.innerHTML = script_kr;
+})
+
+scriptEn.addEventListener('click', ()=>{
+    script.innerHTML = script_en;
+})
 
 window.addEventListener('load', () => {
     window.scrollTo(0, 0);
